46c84a7d1d8d9a26f8d6171749d18332da838dd2
watercereal <watercereal11@gmail.com>
2024-04-07 23:41:12 -0400

    music + folder rename

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Sisyphus Simulator
+## running
+* clone repository 
+* cd into root folder (outside of /game/)
+``` shell     
+    $ java game/main.java
+```   
+
+
+## controls
+controls are handled through terminal text input, multiple inputs can be entered at a time, inputs are executed in the order they are typed
+
+* wasd: move
+* r: refresh map
+
+## music
+* Original: *(https://www.youtube.com/watch?v=jcYvDZKLQJ0&t=0s)*
+* MIDI: *(https://www.youtube.com/watch?v=4BnaipwsQ6M&t=0s)*
+* Soundfont: *(https://musical-artifacts.com/artifacts/23)*
---
+++ game/sound.java
@@ -0,0 +1,43 @@
+package game;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class sound {
+    Long currentFrame;
+    static Clip clip;
+    AudioInputStream audioInputStream;
+
+    static String MUSIC_PATH = "game/sounds/music.wav";
+
+
+    public sound() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        audioInputStream = AudioSystem.getAudioInputStream(new File(MUSIC_PATH).getAbsoluteFile());
+        clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public static void init() {
+        try {
+            sound sound = new sound();
+            sound.play();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    
+
+    }
+
+    public void play(){
+        clip.start();
+    }
+
+    
+}
